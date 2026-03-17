6f3e3096f7e5548fcd931b0ccf39b095be950840
Abega1642 <hei.abega.2@gmail.com>
2026-03-17 13:38:14 +0300

    fix: test(PaymentVerificationRequestedServiceIT): add Transactional annotation to fix failing rollback

--- src/test/java/com/devikapps/vaikaparts/service/PaymentVerificationRequestedServiceIT.java
+++ src/test/java/com/devikapps/vaikaparts/service/PaymentVerificationRequestedServiceIT.java
@@ -36,7 +36,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 class PaymentVerificationRequestedServiceIT extends FacadeIT {
 
   private static final long CONSUMER_PROCESSING_WAIT_MS = 5000L;
